52926e1ce2fb9fe9cf047161d4a18266c09ac4c6
robzdd1922 <robipermanaofficial@gmail.com>
2024-08-28 13:42:39 +0700

    28 agustus 2024

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kontak</title>
+</head>
+<body>
+    <a href="/">Home</a>
+<a href="/blog">blog</a>
+<a href="/about">About</a>
+<a href="/kontak">Contact</a>
+    <h1>Halaman Kontak</h1>
+    <a href="">email</a>
+    <a href="">instagra,</a>
+    <a href="">facebook</a>
+</body>
+</html>
