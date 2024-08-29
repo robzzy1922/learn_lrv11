59f54d64dc5206efd8637e5f8d413eb04625cdc2
robzdd1922 <robipermanaofficial@gmail.com>
2024-08-29 20:56:31 +0700

    29 agustus 2024

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,19 +3,18 @@
 
     {{-- Artikel --}}
 
-    
-
-    <article class="py-8 max-w-screen-md border-b border-gray-500">
-        <h2 class="mb-1 text-3xl tracking-tighter font-bold text-gray-800">Judul Artikel 1</h2>
-        <div class="text-base text-gray-600">
-            <a href="#">Robi Permana</a> | 29 Agustus 2024
-        </div>
-        <p class="my-4 font-light ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet deserunt iure rerum
-            veniam ullam dicta
-            labore
-            expedita aliquam, totam eos. </p>
-            <a href="" class="font-medium text-blue-500 hover:underline hover:underline-offset-2">Read More ></a>
+    @foreach ($posts as $post)
+        <article class="py-8 max-w-screen-md border-b border-gray-500">
+            <a href="/posts/{{ $post['id'] }}" class="hover:underline">
+                <h2 class="mb-1 text-3xl tracking-tighter font-bold text-gray-800 hover:text-gray-950">{{ $post['title'] }}</h2>
+            </a>
+            <div class="text-base text-gray-600">
+                <a href="#">{{ $post['author'] }}</a> | 29 Agustus 2024
+            </div>
+            <p class="my-4 font-light ">{{ Str::limit($post['body'], 100) }}</p>
+            <a href="/posts/{{ $post['id'] }}"
+                class="font-medium text-blue-500 hover:underline hover:underline-offset-2">Read More ></a>
         </article>
-
+    @endforeach
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,9 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' =>
+    return view('posts', ['title' => 'Blog', 'posts' =>[
     [
+        'id'=> 1,
 'title' => 'Judul Artikel 1',
 'author' => 'Robi Permana',
 'body'=> 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet deserunt iure rerum
@@ -21,6 +22,7 @@ Route::get('/posts', function () {
             expedita aliquam, totam eos.'
     ],
     [
+        'id'=> 2,
         'title' => 'Judul Artikel 2',
         'author' => 'Robi Permana',
         'body'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate explicabo
@@ -29,11 +31,42 @@ Route::get('/posts', function () {
             asperiores necessitatibus atque error porro dolores ad officia harum neque.'
             ],
     [
+        'id'=> 3,
         'title' => 'Judul Artikel 3',
         'author' => 'Robi Permana',
         'body'=> 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, fuga.'
-            ],
-]);
+    ],
+]]);
+});
+
+Route::get('/posts/{id}', function($id){
+    $posts =
+    [
+        [
+            'id'=> 1,
+    'title' => 'Judul Artikel 1',
+    'author' => 'Robi Permana',
+    'body'=> 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet deserunt iure rerum
+                veniam ullam dicta
+                labore
+                expedita aliquam, totam eos.'
+        ],
+        [
+            'id'=> 2,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Robi Permana',
+            'body'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate explicabo
+                cupiditate cum, maiores aspernatur suscipit minima nostrum minus fugiat doloribus necessitatibus quos velit
+                deserunt iste nulla saepe hic impedit error. Nihil doloribus totam natus perferendis a cum nemo, qui, minima
+                asperiores necessitatibus atque error porro dolores ad officia harum neque.'
+                ],
+        [
+            'id'=> 3,
+            'title' => 'Judul Artikel 3',
+            'author' => 'Robi Permana',
+            'body'=> 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, fuga.'
+        ],
+    ] ;
 });
 
 Route::get('/kontak', function () {
