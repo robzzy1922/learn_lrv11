de765d96a22cceaa0655261660bdbdca33e20ff5
robzdd1922 <robipermanaofficial@gmail.com>
2024-08-29 19:34:21 +0700

    29 Agustus 2024

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 
 <x-layout>
   <x-slot:title> {{ $title }} </x-slot:title>
-  <p class="font-bold text-4xl bg-gradient-to-r from-blue-300 via-pink-400 to-teal-300 bg-clip-text text-transparent">My name is Robi</p></x-layout>
+  <p class="font-bold text-4xl bg-gradient-to-r from-blue-300 via-pink-400 to-teal-300 bg-clip-text text-transparent">My name is {{ $name }}</p></x-layout>
+ 
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', ['title' => 'Home']);
+    return view('home', ['name' => 'Robi Permana','title' => 'Home']);
 });
 
 Route::get('/about', function () {
@@ -11,7 +11,29 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+    return view('blog', ['title' => 'Blog', 'posts' =>
+    [
+'title' => 'Judul Artikel 1',
+'author' => 'Robi Permana',
+'body'=> 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet deserunt iure rerum
+            veniam ullam dicta
+            labore
+            expedita aliquam, totam eos.'
+    ],
+    [
+        'title' => 'Judul Artikel 2',
+        'author' => 'Robi Permana',
+        'body'=> 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate explicabo
+            cupiditate cum, maiores aspernatur suscipit minima nostrum minus fugiat doloribus necessitatibus quos velit
+            deserunt iste nulla saepe hic impedit error. Nihil doloribus totam natus perferendis a cum nemo, qui, minima
+            asperiores necessitatibus atque error porro dolores ad officia harum neque.'
+            ],
+    [
+        'title' => 'Judul Artikel 3',
+        'author' => 'Robi Permana',
+        'body'=> 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum, fuga.'
+            ],
+]);
 });
 
 Route::get('/kontak', function () {
